55631e6dbc700ff9dc8faf2d2a405deb5550a3c1
Akshai <akshai.inspite@gmail.com>
2026-08-06 10:14:29 +0530

    Configure fixed output asset names and disable cache on index.html, index.js, and index.css

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   server: {
     port: 4201,
     open: true
